b16d68d76aa07debc2a2a84fc9890802320b0b25
mirainsight <miraclec7@gmail.com>
2026-09-22 00:15:24 +0800

    Add Leana, Aaron and Annabelle as pending

--- private/details.js
+++ private/details.js
@@ -44,6 +44,9 @@ window.PARTY_DETAILS = {
     { name: "Ashley Cheah",   status: "pending"},
     { name: "Oswell",         status: "yes"    },
     { name: "Aunty Luna and Uncle Tony (Mira’s parents)", status: "yes"    },
+    { name: "Leana",          status: "pending"},
+    { name: "Aaron",          status: "pending"},
+    { name: "Annabelle",      status: "pending"},
     { name: "Shaun",          status: "heart"  }
   ],
 
